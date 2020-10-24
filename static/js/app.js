b4fee07bd6d76dfd2661e0a8805545e4fb448953
Maria Smith <smith.maria.b@gmail.com>
2020-10-24 09:48:51 -0700

    part one done

--- static/js/app.js
+++ static/js/app.js
@@ -31,15 +31,46 @@ function barAndBubble(){
   d3.json("../../samples.json").then(function(samp) {
     var idSelected = d3.select("#selDataset").property("value");
     var person = samp.samples.filter(person => person.id === idSelected);
-    console.log(person);
+    // console.log(person[0].otu_ids.slice(0,10).map(d=>`OTU-${d}`));
+    // console.log(person[0].otu_ids.slice(0,10).map(String));
     
+    var tableData = samp.metadata.filter(person => person.id == idSelected);
+    var tableLoc = d3.select("#sample-metadata");
+    tableLoc.html("");
+    Object.entries(tableData[0]).forEach(function([key,value]){
+      tableLoc.append('p').text(`${key} : ${value}`)
+    });
+    
+
     var trace = {
-      y: person[0].otu_ids.slice(0,10),
-      x :person[0].sample_values.slice(0,10),
+      y: person[0].otu_ids.slice(0,10).map(d=>`OTU-${d}`).reverse(),
+      x :person[0].sample_values.slice(0,10).reverse(),
       type: "bar",
       orientation: "h"
     };
     Plotly.newPlot("bar",[trace])
+
+    var trace1 = {
+      y: person[0].sample_values,
+      x: person[0].otu_ids,
+      text: person[0].otu_labels,
+      mode: 'markers',
+      marker: {
+        size: person[0].sample_values,
+        color: person[0].otu_ids
+      }
+    };
+    
+    var data = [trace1];
+    
+    var layout = {
+      title: 'Marker Size',
+      showlegend: false,
+      height: 600,
+      width: 1200
+    };
+    
+    Plotly.newPlot('bubble', data, layout);
 
   });
 }
